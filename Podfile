176121c0d87beb48abb01c63fdfa42a040f46cfa
MostafaAbdElFatah <m.abdelfatah@gmail.com>
2019-09-18 15:36:44 +0200

    sliding image and with fullscren options

---
+++ Podfile
@@ -0,0 +1,9 @@
+platform :ios, '9.0'
+
+target 'SlidingImage' do
+  use_frameworks!
+
+  # Pods for SlidingImage
+  pod 'ImageSlideshow', '~> 1.8'
+  pod "ImageSlideshow/SDWebImage"
+end
--- SlidingImage/ViewController.swift
+++ SlidingImage/ViewController.swift
@@ -7,14 +7,49 @@
 //
 
 import UIKit
+import ImageSlideshow
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var slideshow: ImageSlideshow!
+    
+        let sdWebImageSource = [SDWebImageSource(urlString: "https://images.unsplash.com/photo-1432679963831-2dab49187847?w=1080")!, SDWebImageSource(urlString: "https://images.unsplash.com/photo-1447746249824-4be4e1b76d66?w=1080")!, SDWebImageSource(urlString: "https://images.unsplash.com/photo-1463595373836-6e0b0a8ee322?w=1080")!]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+       
+        slideshow.pageIndicatorPosition = .init(horizontal: .center, vertical: .under)
+        slideshow.contentScaleMode = UIViewContentMode.scaleAspectFill
+        
+        let pageControl = UIPageControl()
+        pageControl.currentPageIndicatorTintColor = UIColor.lightGray
+        pageControl.pageIndicatorTintColor = UIColor.black
+        slideshow.pageIndicator = pageControl
+        slideshow.circular = false
+        // optional way to show activity indicator during image load (skipping the line will show no activity indicator)
+        slideshow.activityIndicator = DefaultActivityIndicator()
+        slideshow.delegate = self
+        
+        // can be used with other sample sources as `afNetworkingSource`, `alamofireSource` or `sdWebImageSource` or `kingfisherSource`
+        slideshow.setImageInputs(sdWebImageSource)
+        let recognizer = UITapGestureRecognizer(target: self, action: #selector(ViewController.didTap))
+        slideshow.addGestureRecognizer(recognizer)
+        slideshow.setCurrentPage( 1, animated: true)
     }
+    
+    @objc func didTap() {
+        let fullScreenController = slideshow.presentFullScreenController(from: self)
+        // set the activity indicator for full screen controller (skipping the line will show no activity indicator)
+        fullScreenController.slideshow.activityIndicator = DefaultActivityIndicator(style: .white, color: nil)
+        fullScreenController.slideshow.circular = false
+    }
+}
 
 
+
+extension ViewController: ImageSlideshowDelegate {
+    func imageSlideshow(_ imageSlideshow: ImageSlideshow, didChangeCurrentPageTo page: Int) {
+        print("current page:", page)
+    }
 }
 
